c761ef42b1673938e5641311341e046a9f821bcb
Julia Opeshansky <jopeshan@uoguelph.ca>
2023-04-05 00:29:10 -0400

    Replace recruitEmployee.c

--- src/recruitEmployee.c
+++ src/recruitEmployee.c
@@ -2,12 +2,17 @@
 
 void recruitEmployee (struct employee ** headLL)
 {
-    char yn = 'y';
+    char yn;
     char tempfn[MAX_LENGTH], templn[MAX_LENGTH];
     int count = 0;
-    int empId = 0;
+    int empId;
 
     struct employee *newEmp = (struct employee*)malloc(sizeof(struct employee));
+    if (newEmp == NULL) 
+    {
+        printf("Error: unable to allocate memory for new employee\n");
+        return;
+    }
     printf("Enter the first name of the employee: ");
     scanf("%s", tempfn);
     strcpy(newEmp->fname, tempfn);
@@ -18,23 +23,67 @@ void recruitEmployee (struct employee ** headLL)
 
     printf("\n");
 
+    printf("Enter name of dependent# 1: ");
+    newEmp->numDependents = 1;
+    newEmp->dependents = (char **) malloc(sizeof(char*));
+    newEmp->dependents[0] = (char *) malloc(MAX_LENGTH * sizeof(char));
+    scanf("%s", newEmp->dependents[0]);
+    printf("\n");
+
+    empId = generateId(newEmp->fname, newEmp->lname, *headLL);
+    newEmp->empId = empId;
+
     do
     {
-        count++;
-        printf("Enter the name of dependant #%d:",count);
-        newEmp->dependents[count] = malloc(sizeof(char)*MAX_LENGTH);
-        scanf("%s", newEmp->dependents[count]);
         printf("Do you have any more dependants? ");
+        getchar();
         scanf(" %c", &yn);
+        //yn = getc(stdin);
         yn = tolower(yn);
+        if (yn == 'y')
+        {
+            newEmp->numDependents++;
+            newEmp->dependents = (char **) realloc(newEmp->dependents, newEmp->numDependents * sizeof(char *));
+            if (newEmp->dependents == NULL) 
+            {
+                printf("Error: unable to allocate memory for dependents\n");
+                for (int i = 0; i < newEmp->numDependents-1; i++)
+                {
+                    free(newEmp->dependents[i]);
+                }
+                free(newEmp->dependents);
+                free(newEmp);
+                return;
+            }
+            newEmp->dependents[newEmp->numDependents - 1] = (char *) malloc(MAX_LENGTH * sizeof(char));
+            count++;
+            printf("Enter the name of dependant #%d:",newEmp->numDependents);
+            scanf("%s", newEmp->dependents[count-1]);
+        }
 
         printf("\n");
     }
-    while(yn!= 'n');
+    while(yn == 'y');
 
-    printf("You have %d dependants.\n", count);
-
-    empId = generateId(newEmp->fname, newEmp->lname, *headLL);
     printf("Your computer-generated empId is %d\n", empId);
+
+    // Add new employee node to end of linked list
+    newEmp->nextEmployee = NULL;
+    if (*headLL == NULL) 
+    {
+        *headLL = newEmp;
+    } 
+    else 
+    {
+        struct employee *temp = *headLL;
+        while (temp->nextEmployee != NULL) 
+        {
+            temp = temp->nextEmployee;
+        }
+        temp->nextEmployee = newEmp;
+    }
+
+    printf("You have %d dependants.\n", newEmp->numDependents);
+
 
 }
