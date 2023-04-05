dddc99335145ad388a3b7699c00da19d2be5178f
Julia Opeshansky <jopeshan@uoguelph.ca>
2023-04-05 00:43:59 -0400

    Update lookOnId.c

--- src/lookOnId.c
+++ src/lookOnId.c
@@ -2,24 +2,28 @@
 
 int lookOnId (struct employee * headLL, int whichEmpId)
 {
+    //Check if head is null
     if (headLL == NULL) 
     {
         printf("The linked list is empty.\n");
         return -1;
     }
-
+    //initialize node current, points to head
     struct employee * current = headLL;
     int position = 1;
 
+    //Loops through employee nodes until an employee ID matches the passed ID
     while (current != NULL)
     {
         if (current ->empId == whichEmpId)
         {
+            //If found, return at which position
             return position;
         }
+        //Point to next node
         current = current->nextEmployee;
-        position++;
+        position++; //Increment position
     }
-    return -1;
+    return -1; 
 }
 
