c391568a43ab48d0ea1aa5876e2e5fd1de8273dc
Julia Opeshansky <jopeshan@uoguelph.ca>
2023-04-05 00:27:59 -0400

    Replace mainA3.c

--- src/mainA3.c
+++ src/mainA3.c
@@ -2,9 +2,12 @@
 
 int main (int argc, char *argv[])
 {
-    int choice;
+    int choice, pos, id, idPos, empCount, fired, count;
+    char whichName [MAX_LENGTH];
+    char yn = 'n';
 
     struct employee *headLL = NULL;
+//    a3Emp *head = NULL;
 
     loadEmpData(&headLL, argv[1]);
 
@@ -21,65 +24,174 @@ int main (int argc, char *argv[])
         printf("9. Remove all employees in the current LL\n");
         printf("10. Exit\n");
 
-        printf("Please select an option from the menu :");
-        scanf("%d", &choice);
+        printf("Please select an option from the menu : ");
+        if (scanf("%d", &choice) != 1 || getchar() != '\n') 
+        {
+            while (getchar() != '\n'); //clear input buffer
+            choice = 0; //trigger default
+        }
+
+        printf("\n");
+        
 
         switch (choice)
         {
             case 1: 
 
                 recruitEmployee(&headLL);
+                printf("\n\n");
             
                 break;
             
             case 2:
 
                 printAll(headLL);
+                printf("\n\n");
             
                 break;
             
             case 3:
-                printf("menu option. \n");
-            
+                printf("Enter a position: \n");
+                scanf("%d", &pos);
+                printOne(headLL, pos);
+                printf("\n\n");
                 break;
 
             case 4:
-                printf("menu option. \n");
-            
+                printf("Enter an employee ID: ");
+                scanf("%d", &id);
+                idPos = lookOnId(headLL,id);
+                printf("\n");
+                if (idPos != -1) 
+                {
+                    // Traverse the linked list to get the employee at the given position
+                    struct employee *current = headLL;
+                    for (int i = 1; i < idPos; i++) 
+                    {
+                        current = current->nextEmployee;
+                    }
+
+                    // Print the employee information
+                    printf("Employee id: %d\n", current->empId);
+                    printf("First name: %s\n", current->fname);
+                    printf("Last name: %s\n", current->lname);
+                    printf("Dependents: ");
+                    for (int i = 0; i < current->numDependents; i++) {
+                        printf("%s", current->dependents[i]);
+                        if (i < current->numDependents - 1) 
+                        {
+                            printf(", ");
+                        }
+                    }
+                    printf("\n\n");
+                }
+                else 
+                {
+                    printf("Employee not found with empId %d.\n", id);
+                }
+
                 break;
 
             case 5:
-                printf("menu option. \n");
-            
+                printf("Enter the full name of the employee: \n");
+                fgets(whichName,100,stdin);
+                printf("\n");
+                whichName[strlen(whichName)-1] = '\0';
+                pos = lookOnFullName(headLL, whichName);
+                printf("\n");
+                if (pos != -1) 
+                {
+                    // Traverse list to get the employee at the given position
+                    struct employee *current = headLL;
+                    for (int i = 1; i < pos; i++) 
+                    {
+                        current = current->nextEmployee;
+                    }
+
+                    // Print the employee information
+                    printf("Employee id: %d\n", current->empId);
+                    printf("First name: %s\n", current->fname);
+                    printf("Last name: %s\n", current->lname);
+                    printf("Dependents: ");
+                    for (int i = 0; i < current->numDependents; i++) 
+                    {
+                        printf("%s", current->dependents[i]);
+                        if (i < current->numDependents - 1) 
+                        {
+                            printf(", ");
+                        }
+                    }
+                    printf("\n\n");
+                }
+                else 
+                {
+                    printf("Employee not found with full name %s.\n", whichName);
+                    printf("\n\n");
+                }
                 break;
 
             case 6:
-                printf("menu option. \n");
-            
+                empCount = countEmployees(headLL);
+                printf("\n");
+                printf("Total number of employees = %d", empCount);
+                printf("\n\n");
                 break;
             
             case 7:
-                printf("menu option. \n");
-            
+                printf("After sorting on empId, the employees are as follows: \n\n");
+                sortEmployeesId(headLL);
+                printf("\n\n");
+                
                 break;
             
             case 8:
-                printf("menu option. \n");
+                count = countEmployees(headLL);
+                if (count == -1)
+                {
+                    count = 0;
+                }
+                printf("Currently there are %d employees.\n", count);
+                printf("Which employee do you wish to fire - enter a value between 1 and %d: ", count);
+                scanf("%d", &fired);
+                if (fired < 1 || fired > count)
+                {
+                    printf("Invalid position, try again.\n");
+                    printf("\n\n");
+                }
+                else
+                {
+                    fireOne(&headLL, fired);
+                    count = countEmployees(headLL);
+                    printf("There are now %d employees \n", count);
+                    printf("\n\n");
+                }
             
                 break;
             
             case 9:
-                printf("menu option. \n");
-            
+                printf("Are you sure you want to fire everyone: \n");
+                scanf(" %c", &yn);
+                yn = tolower(yn);
+                if (yn == 'y')
+                {
+                    fireAll(&headLL);
+                }
+                printf("\n\n");
                 break;
             
             case 10:
-                printf("menu option. \n");
+                printf("You've chosen to exit. \n\n");
             
                 break;
+
+            default:
+                printf("Invalid choice. Please try again.");
+                printf("\n\n");
+                break;
+
         }
 
-    } while (scanf("%d", &choice) == 1 && choice != 10);
+    } while (choice != 10);
 
     return 0;
 }
