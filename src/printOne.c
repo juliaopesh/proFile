cf35f469888a59bcf62b4f07ced983f87b2a6413
Julia Opeshansky <jopeshan@uoguelph.ca>
2023-04-05 00:29:44 -0400

    Replace printOne.c

--- src/printOne.c
+++ src/printOne.c
@@ -1,3 +1,40 @@
 #include "../include/headerA3.h"
 
-void printOne (struct employee * headLL, int whichOne);
+void printOne (struct employee * headLL, int whichOne)
+{
+    struct employee * current = headLL;
+    int index = 1;
+
+    if (headLL == NULL) 
+    {
+        printf("The linked list is empty.\n");
+        return;
+    }
+
+    while (current != NULL && index < whichOne)
+    {
+        current = current -> nextEmployee;
+        index++;
+
+    }
+
+    if (current!= NULL && index == whichOne)
+    {
+        printf("\n");
+        printf("\tEmployee ID: %d\n", current->empId);
+        printf("\tFirst name: %s\n", current->fname);
+        printf("\tLast name: %s\n", current->lname);
+        printf("\tNumber of dependents: %d\n", current->numDependents);
+        printf("\tDependents[%d]: ", current -> numDependents);
+        for (int j = 0; j < current-> numDependents; j++)
+        {
+            printf("%s, ", current->dependents[j]);
+        }
+        printf("\n");
+    }
+    else 
+    {
+        printf("Employee not found at position %d.\n", whichOne);
+    }
+    
+}
