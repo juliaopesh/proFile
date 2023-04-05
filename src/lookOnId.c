fece6bc73a4ef7b4ce277dac635d4987fadcb5a9
Julia Opeshansky <jopeshan@uoguelph.ca>
2023-04-05 00:26:59 -0400

    Replace lookOnId.c

--- src/lookOnId.c
+++ src/lookOnId.c
@@ -1,3 +1,25 @@
 #include "../include/headerA3.h"
 
-int lookOnId (struct employee * headLL, int whichEmpId);
+int lookOnId (struct employee * headLL, int whichEmpId)
+{
+    if (headLL == NULL) 
+    {
+        printf("The linked list is empty.\n");
+        return -1;
+    }
+
+    struct employee * current = headLL;
+    int position = 1;
+
+    while (current != NULL)
+    {
+        if (current ->empId == whichEmpId)
+        {
+            return position;
+        }
+        current = current->nextEmployee;
+        position++;
+    }
+    return -1;
+}
+
