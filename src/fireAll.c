5395ffb30c3f72486799c0d8642c8aa3a6504f59
Julia Opeshansky <jopeshan@uoguelph.ca>
2023-04-05 00:26:30 -0400

    Replace fireAll.c

--- src/fireAll.c
+++ src/fireAll.c
@@ -1,3 +1,22 @@
 #include "../include/headerA3.h"
 
-void fireAll (a3Emp ** headLL);
+void fireAll (a3Emp ** headLL)
+{
+    if (*headLL == NULL) 
+    {
+        printf("The linked list is empty.\n");
+        return;
+    }
+    
+    a3Emp *current = *headLL;
+    while (current != NULL) 
+    {
+        //point to next node so that we can access the rest of the list
+        a3Emp *next = current->nextEmployee;
+        free(current); //free each node
+        current = next; //set current to next
+    }
+    // set the head to NULL to indicate an empty list
+    *headLL = NULL; 
+    printf ("All fired. Linked list is now empty.\n");
+}
