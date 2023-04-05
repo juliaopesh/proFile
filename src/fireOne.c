286a1348792df62d6336fcb86acbe72ba07f7c18
Julia Opeshansky <jopeshan@uoguelph.ca>
2023-04-05 00:28:28 -0400

    Replace fireOne.c

--- src/fireOne.c
+++ src/fireOne.c
@@ -1,4 +1,39 @@
 #include "../include/headerA3.h"
 
-void fireOne (a3Emp ** headLL, int whichOne);
+void fireOne (a3Emp ** headLL, int whichOne)
+{
+
+    if (*headLL == NULL) 
+    {
+        printf("The linked list is empty.\n");
+        return;
+    }
+
+    //Nodes
+    a3Emp * current = *headLL;
+    a3Emp * prev = NULL;
+
+    int index = 1;
+
+    //Check if the employee to be fired is at the head of the list
+
+    if (whichOne == 1)
+    {
+        *headLL = current ->nextEmployee;
+    }
+    else
+    {
+        //Loop through list to count employees
+        while (index != whichOne)
+        {
+            prev = current;
+            current = current -> nextEmployee;
+            index++;
+        }
+        prev->nextEmployee = current->nextEmployee;
+    }
+    
+    printf("Employee [Id: %d] fired.\n", current->empId);
+    free(current);
+}
 
